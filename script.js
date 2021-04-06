18f0071ab8f4ef9df8205433220d8587420c4699
NCYU-DEDM-exhibition <81426036+NCYU-DEDM-exhibition@users.noreply.github.com>
2021-04-06 15:14:36 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 /*-----錨點-----*/
 $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      if (target.length) {
-        $("html, body").animate(
-          {
-            scrollTop: target.offset().top
-          },
-          1000
-        );
-        return false;
-      }
-    }
-  });
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate({
+                        scrollTop: target.offset().top
+                    },
+                    1000
+                );
+                return false;
+            }
+        }
+    });
 });
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 500) {
+        $('#bar_bg').fadeIn();
+    } else {
+        $('#bar_bg').fadeOut();
+    }
+});
+
+function openLink(evt, animName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    }
+    document.getElementById(animName).style.display = "block";
+    evt.currentTarget.className += " w3-red";
+}
